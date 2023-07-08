dfa8f597c708dd88eec3992a3ea829a9614fa67d
KupoKopu <lukaskuzavas@gmail.com>
2023-07-08 18:57:48 +0100

    Add GoogleTest framework

---
+++ cmake-build-debug/google-tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/LMLK-404/Documents/projects/ConsoleTetris/google-tests/lib
+# Build directory: C:/Users/LMLK-404/Documents/projects/ConsoleTetris/cmake-build-debug/google-tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/google-tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/LMLK-404/Documents/projects/ConsoleTetris/google-tests/lib/googletest
+# Build directory: C:/Users/LMLK-404/Documents/projects/ConsoleTetris/cmake-build-debug/google-tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
