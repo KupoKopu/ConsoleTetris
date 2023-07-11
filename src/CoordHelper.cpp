21651a25c1cf956e31fa2e98abf03436ba7ff77e
KupoKopu <lukaskuzavas@gmail.com>
2023-07-11 23:17:43 +0100

    CT02b: add rotation function for pieces

--- src/CoordHelper.cpp
+++ src/CoordHelper.cpp
@@ -7,3 +7,21 @@ int renderOnTo(wchar_t * source, int sourceWidth, int sourceHeight, const wchar_
     }
     return 0;
 }
+
+int rotate(int pixelX, int pixelY, int rotation) {
+    switch (rotation % 4) {
+        case 0: {
+            return pixelY * 4 + pixelX;
+        }
+        case 1: {
+            return 12 + pixelY - (pixelX * 4);
+        }
+        case 2: {
+            return 15 - (pixelY * 4) - pixelX;
+        }
+        case 3: {
+            return 3 - pixelY + (pixelX * 4);
+        }
+        return 0;
+    }
+}
--- src/CoordHelper.h
+++ src/CoordHelper.h
@@ -2,4 +2,5 @@
 #define CONSOLETETRIS_COORDHELPER_H
 #include <utility>
 int renderOnTo(wchar_t * source, int sourceWidth, int sourceHeight, const wchar_t * copy, int copyWidth, int copyHeight, std::pair<int, int> offset);
+int rotate(int pixelX, int pixelY, int rotation);
 #endif //CONSOLETETRIS_COORDHELPER_H
