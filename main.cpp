42d926ca0879d16a6b968bb1f30b7447905c0725
KupoKopu <lukaskuzavas@gmail.com>
2023-07-11 08:50:10 +0100

    CT02a: add PlayArea class

--- main.cpp
+++ main.cpp
@@ -2,24 +2,42 @@
 #include <thread>
 #include <chrono>
 #include "src/Terminal.h"
+#include "src/PlayArea.h"
+
 short screenWidth = 80;
 short screenHeight = 30;
 using namespace std::chrono_literals;
 
-int fieldWidth = screenWidth;
-int fieldHeight = screenHeight;
+int fieldWidth = 12;
+int fieldHeight = 18;
 unsigned char *playField = nullptr;
+
+int renderOnTo(wchar_t * source, int sourceWidth, int sourceHeight, wchar_t * copy, int copyWidth, int copyHeight) {
+    for (int x = 0; x < copyWidth; x++)
+    {
+        for (int y = 0; y < copyHeight; y++)
+        {
+            source[(y + 2)*sourceWidth + (x + 77)] = L" ABCDEFG=#"[copy[y*copyWidth + x]];
+        }
+    }
+    return 0;
+}
 
 int main() {
     Terminal *terminal = Terminal::getInstance(screenWidth, screenHeight);
+    PlayArea *playArea = new PlayArea(12, 18);
 
-    playField = new unsigned char[fieldWidth * fieldHeight]; // Create play field buffer
-    for (int x = 0; x < fieldWidth; x++) // Board Boundary
+
+    wchar_t *screen = new wchar_t[screenWidth * screenHeight];
+    for (int i = 0; i < screenWidth * screenHeight; i++) screen[i] = L' ';
+
+    for (int x = 0; x < fieldWidth; x++)
+    {
         for (int y = 0; y < fieldHeight; y++)
-            playField[y * fieldWidth + x] = (x == 0 || x == fieldWidth - 1 || y == fieldHeight - 1) ? 9 : 0;
-
-    wchar_t *screen = new wchar_t[screenWidth*screenHeight];
-    for (int i = 0; i < screenWidth*screenHeight; i++) screen[i] = L'#';
+        {
+            screen[(y + 2)*screenWidth + (x + 77)] = L" ABCDEFG=#"[playArea->getArea()[y*fieldWidth + x]];
+        }
+    }
 
     terminal->render(screen);
 
