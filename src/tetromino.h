7616caac42ecdc6363d002616eab16c3220939cf
KupoKopu <lukaskuzavas@gmail.com>
2023-07-12 00:47:35 +0100

    CT02b: fix incorrect tetromino initialization

--- src/tetromino.h
+++ src/tetromino.h
@@ -2,6 +2,12 @@
 #define CONSOLETETRIS_TETROMINO_H
 #include <string>
 
-const std::wstring tetromino[7];
+const std::wstring tetromino[7] = {L"..X...X...X...X.",
+                                   L"..X..XX...X.....",
+                                   L".....XX..XX.....",
+                                   L"..X..XX..X......",
+                                   L".X...XX...X.....",
+                                   L".X...X...XX.....",
+                                   L"..X...X..XX....."};
 
 #endif //CONSOLETETRIS_TETROMINO_H
