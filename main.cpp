7342b18bfd36085a2e726a21a792dc57f26b58cc
KupoKopu <lukaskuzavas@gmail.com>
2023-07-12 19:36:26 +0100

    refacto Main class

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
-#include <iostream>
-#include <thread>
-#include <chrono>
 #include <vector>
-#include "src/Terminal.h"
+
 #include "src/constants.h"
 #include "src/Tetris.h"
 #include "src/Menu.h"
 
-using namespace std::chrono_literals;
-
 int main() {
+    // control state of application
     ExitCode exitCode = OK;
 
-    Terminal *terminal = Terminal::getInstance(SCREEN_WIDTH, SCREEN_HEIGHT);
-    HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
     Tetris *tetris = new Tetris(SCREEN_WIDTH, SCREEN_HEIGHT, FIELD_WIDTH, FIELD_HEIGHT, exitCode);
     Menu *menu = new Menu(SCREEN_WIDTH, SCREEN_HEIGHT, exitCode);
     Frame* activeFrame = menu;
@@ -34,15 +28,5 @@ int main() {
             continue;
         }
 
-    }
-
-    if (exitCode == RETURN) {
-        SetConsoleActiveScreenBuffer(stdOut);
-    }
-
-    while (exitCode == RETURN) {
-        std::cout << exitCode;
-        std::this_thread::sleep_for(5000ms);
-        exitCode = EXIT;
     }
 }
