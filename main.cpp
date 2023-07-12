9ca5994fa2cb4622a50cd7006428a4dcebd0a1ac
KupoKopu <lukaskuzavas@gmail.com>
2023-07-12 17:38:17 +0100

    CT01a: implement Menu into main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "src/Terminal.h"
 #include "src/constants.h"
 #include "src/Tetris.h"
+#include "src/Menu.h"
 
 using namespace std::chrono_literals;
 
@@ -14,10 +15,26 @@ int main() {
     Terminal *terminal = Terminal::getInstance(SCREEN_WIDTH, SCREEN_HEIGHT);
     HANDLE stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
     Tetris *tetris = new Tetris(SCREEN_WIDTH, SCREEN_HEIGHT, FIELD_WIDTH, FIELD_HEIGHT, exitCode);
-    Frame* activeFrame = tetris;
+    Menu *menu = new Menu(SCREEN_WIDTH, SCREEN_HEIGHT, exitCode);
+    Frame* activeFrame = menu;
 
+    while (exitCode != EXIT) {
+        if (OK == exitCode) {
+            activeFrame->start();
+            continue;
+        }
+        if (START == exitCode) {
+            activeFrame = tetris;
+            exitCode = OK;
+            continue;
+        }
+        if (RETURN == exitCode) {
+            activeFrame = menu;
+            exitCode = OK;
+            continue;
+        }
 
-    activeFrame->start();
+    }
 
     if (exitCode == RETURN) {
         SetConsoleActiveScreenBuffer(stdOut);
